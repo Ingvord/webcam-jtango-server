7f21c131420fbe5be3b2ed5513eae00f06dc5ed0
Igor Khokhriakov <mail@ingvord.ru>
2015-04-07 15:15:34 +0200

    progress

--- src/main/java/hzg/wpn/tango/camera/webcam/Pi4JPlayerImpl.java
+++ src/main/java/hzg/wpn/tango/camera/webcam/Pi4JPlayerImpl.java
@@ -14,25 +14,56 @@ import java.util.Properties;
  * @since 03.04.2015
  */
 public class Pi4JPlayerImpl implements Player {
-//    private JvPi jvpi;
+    public static final String PI4_J_PROPERTIES = "/Pi4J.properties";
+    //    private JvPi jvpi;
 private static final Logger logger = LoggerFactory.getLogger(Pi4JPlayerImpl.class);
-
-    private final String width = "960";
-    private final String height = "720";
-    private final String timeout = "0";
-    private final String rotation = "180";
-    //    private final String timelapse = "100";
-    private final String encoding = "jpg";
-    private final String output = "capture.jpeg";
-    private final String quality = "75";
-    // Remember to add filename and extension!
-    private final String captureInstruction = "/usr/bin/raspistill -h " + height + " -w " + width + " -rot " + rotation +
-            " -e " + encoding + " -t " + timeout + /*" -tl " + timelapse +*/ " -q " + quality + " -o " + output;
     private final String killInstruction = "killall raspistill";
+    private String mode = "0";
+    private String width = "960";
+    private String height = "720";
+    private String timeout = "0";
+    private String rotation = "180";
+    //    private final String timelapse = "100";
+    private String encoding = "jpg";
+    private String quality = "75";
+    private String output = "capture.jpeg";
+    private String[] supportedFormats;
 
     @Override
     public void init(Properties webcamProperties) throws Exception {
-        //TODO
+        for (String webcamProperty : webcamProperties.stringPropertyNames()) {
+            switch (webcamProperty) {
+                case "Pi4JPlayerImpl.width":
+                    width = webcamProperties.getProperty(webcamProperty);
+                    break;
+                case "Pi4JPlayerImpl.height":
+                    height = webcamProperties.getProperty(webcamProperty);
+                    break;
+                case "Pi4JPlayerImpl.rotation":
+                    rotation = webcamProperties.getProperty(webcamProperty);
+                    break;
+                case "Pi4JPlayerImpl.timeout":
+                    timeout = webcamProperties.getProperty(webcamProperty);
+                    break;
+                case "Pi4JPlayerImpl.encoding":
+                    encoding = webcamProperties.getProperty(webcamProperty);
+                    break;
+                case "Pi4JPlayerImpl.quality":
+                    quality = webcamProperties.getProperty(webcamProperty);
+                    break;
+            }
+        }
+
+        Properties pi4jProperties = new Properties();
+        pi4jProperties.load(getClass().getResourceAsStream(PI4_J_PROPERTIES));
+
+        int supportedFormatsSize = Integer.parseInt(pi4jProperties.getProperty("mode.total"));
+        supportedFormats = new String[supportedFormatsSize];
+
+        for (int i = 0; i < supportedFormatsSize; ++i) {
+            String mode = pi4jProperties.getProperty("mode." + i);
+            supportedFormats[i] = mode;
+        }
     }
 
     @Override
@@ -43,9 +74,9 @@ private static final Logger logger = LoggerFactory.getLogger(Pi4JPlayerImpl.clas
     public BufferedImage capture() throws Exception {
         Runtime r = Runtime.getRuntime();
 
-        logger.info("Executing: " + this.captureInstruction);
+        logger.info("Executing: " + getCaptureInstruction());
 
-        r.exec(this.captureInstruction);
+        r.exec(getCaptureInstruction());
 
         return ImageIO.read(new File(output));
     }
@@ -61,8 +92,7 @@ private static final Logger logger = LoggerFactory.getLogger(Pi4JPlayerImpl.clas
 
     @Override
     public String[] supportedFormats() {
-        //TODO return supported formats
-        return new String[0];
+        return supportedFormats;
     }
 
     @Override
@@ -73,7 +103,7 @@ private static final Logger logger = LoggerFactory.getLogger(Pi4JPlayerImpl.clas
 
     @Override
     public void setFormat(int id) throws Exception {
-        //TODO
+        mode = Integer.toString(id + 1);
     }
 
     @Override
@@ -83,5 +113,10 @@ private static final Logger logger = LoggerFactory.getLogger(Pi4JPlayerImpl.clas
         } catch (Exception e) {
             throw new IOException(e);
         }
+    }
+
+    private String getCaptureInstruction() {
+        return "/usr/bin/raspistill -h " + height + " -w " + width + " -rot " + rotation +
+                " -md " + mode + " -e " + encoding + " -t " + timeout + /*" -tl " + timelapse +*/ " -q " + quality + " -o " + output;
     }
 }
