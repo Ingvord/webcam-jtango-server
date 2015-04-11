ecad66520169990055b5a72d8538a5b00f9d5edf
Ingvord <tla-ingvord@yandex.ru>
2015-04-12 00:48:35 +0300

    progress

--- src/main/java/hzg/wpn/tango/camera/webcam/JavaCVPlayerImpl.java
+++ src/main/java/hzg/wpn/tango/camera/webcam/JavaCVPlayerImpl.java
@@ -2,6 +2,8 @@ package hzg.wpn.tango.camera.webcam;
 
 import org.bytedeco.javacpp.opencv_core;
 import org.bytedeco.javacv.OpenCVFrameGrabber;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.awt.image.BufferedImage;
 import java.io.IOException;
@@ -12,11 +14,18 @@ import java.util.Properties;
  * @since 10.04.2015
  */
 public class JavaCVPlayerImpl implements Player {
-    private final int frameWidth = 800;
-    private final int frameHeight = 600;
+    private static final Logger logger = LoggerFactory.getLogger(JavaCVPlayerImpl.class);
+
+    private final String[] supportedFormats = {
+            "640x480",
+            "800x600",
+            "1280x720"
+    };
+
+    private int frameWidth = 800;
+    private int frameHeight = 600;
     private final OpenCVFrameGrabber grabber = new OpenCVFrameGrabber(0);
     //    private final CanvasFrame frame = new CanvasFrame("Web Cam");
-    private volatile boolean running = false;
     private volatile BufferedImage bufImg;
 
     public JavaCVPlayerImpl() {
@@ -24,70 +33,65 @@ public class JavaCVPlayerImpl implements Player {
 
     @Override
     public void init(Properties webcamProperties) throws Exception {
-        //TODO
+        for (String webcamProperty : webcamProperties.stringPropertyNames()) {
+            switch (webcamProperty) {
+                case "JavaCVPlayerImpl.width":
+                    frameWidth = Integer.parseInt(webcamProperties.getProperty(webcamProperty));
+                    break;
+                case "JavaCVPlayerImpl.height":
+                    frameHeight = Integer.parseInt(webcamProperties.getProperty(webcamProperty));
+                    break;
+            }
+        }
     }
 
     @Override
     public void start() throws Exception {
-        //TODO thread pool
-        running = true;
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    grabber.setImageWidth(frameWidth);
-                    grabber.setImageHeight(frameHeight);
-                    grabber.start();
-                    while (running) {
-
-                        opencv_core.IplImage cvimg = grabber.grab();
-                        if (cvimg != null) {
-
-                            // cvFlip(cvimg, cvimg, 1); // mirror
-
-                            // show image on window
-
-                            bufImg = cvimg.getBufferedImage();
-//                            frame.showImage(bufImg);
-                        }
-                    }
-                    grabber.stop();
-                    grabber.release();
-//                    frame.dispose();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        }).start();
+        grabber.setImageWidth(frameWidth);
+        grabber.setImageHeight(frameHeight);
+        grabber.start();
     }
 
     @Override
     public BufferedImage capture() throws Exception {
+        opencv_core.IplImage cvimg = grabber.grab();
+        if (cvimg != null) {
+            bufImg = cvimg.getBufferedImage();
+        }
         return bufImg;
     }
 
     @Override
     public void stop() throws Exception {
-        running = false;
+        grabber.stop();
+        grabber.release();
     }
 
     @Override
     public String[] supportedFormats() {
-        throw new UnsupportedOperationException("This method is not supported in " + this.getClass());
+        return supportedFormats;
     }
 
     @Override
     public String currentFormat() {
-        throw new UnsupportedOperationException("This method is not supported in " + this.getClass());
+        return grabber.getFormat();
     }
 
     @Override
     public void setFormat(int id) throws Exception {
-        throw new UnsupportedOperationException("This method is not supported in " + this.getClass());
+        String format = supportedFormats[id];
+        String[] dims = format.split("x");
+        frameWidth = Integer.parseInt(dims[0]);
+        frameHeight = Integer.parseInt(dims[1]);
     }
 
     @Override
     public void close() throws IOException {
-
+        try {
+            stop();
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+            throw new IOException(e);
+        }
     }
 }
