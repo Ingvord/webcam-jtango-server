0d0fdfa011e26f775fbb524624b2e2f99d957836
Igor Khokhriakov <mail@ingvord.ru>
2015-04-14 17:23:35 +0200

    replace Xuggler with JavaCV

--- src/main/java/hzg/wpn/tango/camera/webcam/JavaCVPlayerImpl.java
+++ src/main/java/hzg/wpn/tango/camera/webcam/JavaCVPlayerImpl.java
@@ -8,12 +8,15 @@ import org.slf4j.LoggerFactory;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.Properties;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author Igor Khokhriakov <igor.khokhriakov@hzg.de>
  * @since 10.04.2015
  */
-public class JavaCVPlayerImpl implements Player {
+public class JavaCVPlayerImpl implements Player, Runnable {
     private static final Logger logger = LoggerFactory.getLogger(JavaCVPlayerImpl.class);
 
     private final String[] supportedFormats = {
@@ -21,12 +24,13 @@ public class JavaCVPlayerImpl implements Player {
             "800x600",
             "1280x720"
     };
-
+    private final OpenCVFrameGrabber grabber = new OpenCVFrameGrabber(0);
+    private final ExecutorService exec = Executors.newSingleThreadExecutor();
     private int frameWidth = 800;
     private int frameHeight = 600;
-    private final OpenCVFrameGrabber grabber = new OpenCVFrameGrabber(0);
     //    private final CanvasFrame frame = new CanvasFrame("Web Cam");
     private volatile BufferedImage bufImg;
+    private volatile boolean running;
 
     public JavaCVPlayerImpl() {
     }
@@ -45,26 +49,39 @@ public class JavaCVPlayerImpl implements Player {
         }
     }
 
+    public void run() {
+        try {
+            grabber.setImageWidth(frameWidth);
+            grabber.setImageHeight(frameHeight);
+            grabber.start();
+            while (running) {
+                opencv_core.IplImage cvimg = grabber.grab();
+                if (cvimg != null) {
+                    bufImg = cvimg.getBufferedImage();
+                }
+            }
+            grabber.stop();
+            grabber.release();
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
     @Override
     public void start() throws Exception {
-        grabber.setImageWidth(frameWidth);
-        grabber.setImageHeight(frameHeight);
-        grabber.start();
+        running = true;
+        exec.submit(this);
     }
 
     @Override
     public BufferedImage capture() throws Exception {
-        opencv_core.IplImage cvimg = grabber.grab();
-        if (cvimg != null) {
-            bufImg = cvimg.getBufferedImage();
-        }
         return bufImg;
     }
 
     @Override
     public void stop() throws Exception {
-        grabber.stop();
-        grabber.release();
+        running = false;
     }
 
     @Override
@@ -89,6 +106,8 @@ public class JavaCVPlayerImpl implements Player {
     public void close() throws IOException {
         try {
             stop();
+            exec.awaitTermination(3000, TimeUnit.MILLISECONDS);
+            exec.shutdown();
         } catch (Exception e) {
             logger.error(e.getMessage(), e);
             throw new IOException(e);
